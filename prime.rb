8e42ec72297102eb3daec7d49b61a4997a3d9769
global-listener-7039 <jodiesternal@yahoo.com>
2020-09-18 16:10:11 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,13 +2,8 @@ def prime?(x)
   if x < 2
     return false
   else x < 1
-    return false
-  else
-  for y in 2..(x-1)
-    if (x % y) == 0
-      return true
-    end
-  end
+    (2...)
+
 end
 
   true
